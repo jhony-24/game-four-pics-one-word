7bb4b0cb497352cfef3f11cb4d294ff8709f857c
jhony <jhonyvega24@gmail.com>
2020-04-23 19:04:38 -0500

    button to see mobile mode on desktop

--- src/components/atoms/SEO/SEO.jsx
+++ src/components/atoms/SEO/SEO.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
+import FloatingButton from '../FloatingButton/FloatingButton';
+import { IoIosOpen } from 'react-icons/io';
 
 const SEO = ({ children, ...props }) => {
   return (
@@ -8,8 +10,22 @@ const SEO = ({ children, ...props }) => {
       <div className="root">
         {children}
       </div>
+      <ButtonOpenResize />
     </Fragment>
   )
+}
+
+// test
+// button to view game in desktop in mode phone
+function ButtonOpenResize() {
+  const openWindow = () => {
+    let location = window.location;
+    let title = document.title;
+    let target = '_blank';
+    let features = 'width=300,height=400,scrollbars=NO,top=0,status=no';
+    window.open(location, title, target, features);
+  }
+  return <FloatingButton onClick={() => openWindow()} ><IoIosOpen size={20} /></FloatingButton>
 }
 
 export default SEO;
