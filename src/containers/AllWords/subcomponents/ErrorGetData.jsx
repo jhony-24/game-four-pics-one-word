a973472d26aa1e745bb02cdfe627ec755b4adeaa
jhony <jhonyvega24@gmail.com>
2020-04-24 12:10:08 -0500

    set button icon to reload page

--- src/containers/AllWords/subcomponents/ErrorGetData.jsx
+++ src/containers/AllWords/subcomponents/ErrorGetData.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 import { StyleSheet, css } from "aphrodite/lib";
 import Text from "src/components/atoms/Text/Text";
+import { AiOutlineSync } from "react-icons/ai";
 
 
 const styles = StyleSheet.create({
-    container : {
-        margin : "auto",
-        position : "fixed",
-        top : "50%",
-        left : "50%",
-        transform : "translate(-50%,-50%)",
-        textAlign : "center",
-        background : "white",
-        padding : "2em",
-        borderRadius : "20px",
-        "@media screen and (max-width:400px)" : {
-            width :"80%"
+    container: {
+        margin: "auto",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+        textAlign: "center",
+        background: "white",
+        padding: "2em",
+        borderRadius: "20px",
+        "@media screen and (max-width:400px)": {
+            width: "80%"
         }
     },
-    title : {
-        marginBottom : "20px",
-        color : "rgb(60,60,60)"
+    title: {
+        marginBottom: "20px",
+        color: "rgb(60,60,60)"
     }
 })
 
 
 const ErrorGetData = () => {
-    return(
+    return (
         <div className={css(styles.container)}>
             <h2 className={css(styles.title)} >Ups! A ocurrido un error</h2>
             <Text color="gray">los datos no se obtuvieron correctamente</Text>
+            <br />
+            <a href={window.location}>
+                <AiOutlineSync size={25} /> 
+                <Text>reload</Text>
+            </a>
         </div>
     )
 }
