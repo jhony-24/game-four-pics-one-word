ea0e923ae04af94b2d3a6af1ba6439c7d097a95d
jhony <jhonyvega24@gmail.com>
2020-04-24 11:07:33 -0500

    created button to view in mobile to the desktop

---
+++ src/components/molecules/ButtonViewInMobile/ButtonViewInMobile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import FloatingButton from 'src/components/atoms/FloatingButton';
+import { IoIosOpen } from 'react-icons/io';
+import { Portal } from 'react-portal';
+import useResize from 'src/use/useResize';
+
+const ButtonViewInMobile = () => {
+
+  const [width] = useResize();
+
+  const isMobile = () => width <= 400;
+  
+  const openWindow = () => {
+    let location = window.location;
+    let title = document.title;
+    let target = '_blank';
+    let features = 'width=300,height=400,scrollbars=NO,top=0,status=no';
+    window.open(location, title, target, features);
+  }
+
+  if (!isMobile()) {
+    return (
+      <Portal>
+        <FloatingButton onClick={() => openWindow()} ><IoIosOpen size={20} /></FloatingButton>
+      </Portal>
+    )
+  }
+  return null;
+
+}
+
+export default ButtonViewInMobile;
