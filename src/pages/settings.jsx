d96661bf1d401efab276bd3762184ac0f23cdd55
jhony <jhonyvega24@gmail.com>
2020-04-25 17:46:05 -0500

    add library moment js

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import Fade from "src/components/dom/Fade";
 import CheckLogged from "src/hoc/CheckLogged"
 import SEO from "src/components/atoms/SEO"
+import ViewSettings from "src/views/ViewSettings/ViewSettings";
 
 const Settings = () => {
     return (
         <SEO title="my settings" >
             <div className="settings">
-
+                <ViewSettings/>
             </div>
         </SEO>
     )
