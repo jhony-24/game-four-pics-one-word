faa36d134eb3f197e79b9f751f05b3c8329dc72c
jhony <jhonyvega24@gmail.com>
2020-04-26 11:33:24 -0500

    add items for basic configuration

--- src/views/ViewSettings/ViewSettings.jsx
+++ src/views/ViewSettings/ViewSettings.jsx
@@ -4,27 +4,65 @@ import RowItem from "src/components/molecules/RowItem"
 import ButtonBack from 'src/components/molecules/ButtonBack';
 import style from './style';
 import { FaCog } from 'react-icons/fa';
-import IconNatural from 'src/components/atoms/IconNatural/IconNatural';
+import IconNatural from 'src/components/atoms/IconNatural';
+import Avatar from 'src/components/atoms/Avatar/Avatar';
+import Paragraph from 'src/components/molecules/Paragraph';
+import { TEXT_PRIMARY } from 'src/tools/constants';
+import { IoIosMoon, IoIosMusicalNote, IoIosText } from 'react-icons/io';
+import TextField from 'src/components/atoms/TextField';
 
 const ViewSettings = () => {
 
   const header = { ...style.header._definition };
-  const  textHeader = css(style.textHeader);
+  const textHeader = css(style.textHeader);
+  const colorLight = TEXT_PRIMARY;
 
   return (
     <div className="settings">
       <RowItem
         styles={header}
-        leftBody={
-          <ButtonBack color="white" />
-        }
-        centerBody = {
-        <p className={textHeader}>Configuraciones</p>
-        }
+        leftBody={<ButtonBack color={colorLight} />}
+        centerBody={<p className={textHeader}>Configuraciones</p>}
         rightBody={
-          <IconNatural text="" icon={<FaCog/>} coloricon="white" />
+          <IconNatural
+            icon={<FaCog />}
+            coloricon={colorLight}
+          />}
+      />
+
+      <RowItem
+        styles={{ padding: "20px 15px" }}
+        leftBody={<Avatar src="https://image.shutterstock.com/image-photo/casually-handsome-confident-young-man-260nw-439433326.jpg" />}
+        centerBody={
+          <Paragraph
+            title="Cambiar mi nombre"
+            detail="10 publicaciones" colordetail="gray" />
         }
-        />
+      />
+
+      <RowItem
+        styles={{ padding: "20px 15px" }}
+        leftBody={<Avatar color="rgb(60,60,60)" icon={<IoIosMoon color="white" />} />}
+        centerBody={<Paragraph title="dark mode" />}
+      />
+
+      <RowItem
+        styles={{ padding: "20px 15px" }}
+        leftBody={<Avatar color="orange" icon={<IoIosMusicalNote color="white" />} />}
+        centerBody={<Paragraph title="active sounds" />}
+      />
+
+      <RowItem
+        styles={{ padding: "20px 15px" }}
+        leftBody={<Avatar color="rgb(30,130,230)" icon={<IoIosText color="white" />} />}
+        centerBody={
+          <Paragraph
+            title="Cambiar mi nombre"
+          />
+        }
+      />
+
+
     </div>
   );
 
