0c6aa39f011e88a8721b5764c67a0e66515e9502
jhony <jhonyvega24@gmail.com>
2020-05-01 19:59:09 -0500

    new duck created 'navigation'

--- src/ducks/navigation/actions.js
+++ src/ducks/navigation/actions.js
@@ -1,3 +1,7 @@
 import { createAction } from "redux-actions";
 
 const base = "ducks/navigation";
+
+// actions
+export const activeSound = createAction(`${base}/ACTIVE_SOUND`);
+export const deactiveSound = createAction(`${base}/DEACTIVE_SOUND`);
--- src/ducks/navigation/reducers.js
+++ src/ducks/navigation/reducers.js
@@ -1,14 +1,24 @@
 import { handleActions } from "redux-actions";
-
+import * as actions from "./actions";
+import * as operations from "./operations";
 
 const initialState = {
-
+    useSound: true
 }
-
 
 const handlers = {
-
+    [actions.activeSound]: (state) => {
+        return {
+            ...state,
+            useSound: true
+        }
+    },
+    [actions.deactiveSound]: (state) => {
+        return {
+            ...state,
+            useSound: false
+        }
+    }
 }
-
 
 export default handleActions(handlers, initialState);
