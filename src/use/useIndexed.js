44e851e3ac0897ba1faec05e7842eae70060543c
jhony <jhonyvega24@gmail.com>
2020-05-04 16:49:28 -0500

    create crud into indexedDb

--- src/use/useIndexed.js
+++ src/use/useIndexed.js
@@ -1,32 +1,50 @@
 import { useState } from "react"
 
 const useIndexed = () => {
-    const [data, setData] = useState();
+    const [data, setData] = useState({});
+    const [type, setType] = useState(null);
+
+    const CREATE = "CREATE", UPDATE = "UPDATE", REMOVE = "REMOVE", READ = "READ";
+
     let db = window.indexedDB.open("game", 1);
 
     db.onupgradeneeded = (e) => {
-
+        db.createObjectStore("user", { keyPath: "key" });
     }
+
     db.onsuccess = (e) => {
-
     }
 
-    const read = () => {
-
-    }
-    const update = () => {
-
-    }
-    const remove = () => {
-
+    const read = (key) => {
+        let transaction = db.transaction(["user"], "readonly");
+        let store = transaction.objectStore("user");
+        store.get(key);
     }
 
-    return {
-        data,
-        read,
-        update,
-        remove,
+    const update = (data) => {
+        let transaction = db.transaction(["user"], "readwrite");
+        let store = transaction.objectStore("user");
+        store.put(data);
     }
+
+    const remove = (key) => {
+        let transaction = db.transaction(["user"], "readwrite");
+        let store = transaction.objectStore("user");
+        store.remove(key);
+    }
+    const create = (data) => {
+        let transaction = db.transaction(["user"], "readwrite");
+        let store = transaction.objectStore("user");
+        store.add(data);
+    }
+
+    // return {
+    //     data,
+    //     read,
+    //     update,
+    //     remove,
+    //     create,
+    // }
 }
 
 export default useIndexed;
