c95da0ac554d6942358bf3996fdb3628939c5032
jhony <jhonyvega24@gmail.com>
2020-05-05 20:02:26 -0500

    order the most ranked in triangular order

--- src/ducks/word/selectors.js
+++ src/ducks/word/selectors.js
@@ -3,6 +3,13 @@ export const getWordsTopRanked = (state, totalInRanking = 3) => {
     let orderByMorePoints = cloneArray.sort((a, b) => {
         return b.points - a.points;
     });
-    let topRanked = orderByMorePoints.splice(0, totalInRanking);
+
+    let topRanked = orderByMorePoints.splice(0, totalInRanking).reverse();
+    let meanIndex = Math.floor(topRanked.length / 2);
+    let middle = topRanked[meanIndex];
+
+    topRanked[meanIndex] = topRanked[topRanked.length - meanIndex];
+    topRanked[topRanked.length - meanIndex] = middle;
+
     return topRanked;
 }
