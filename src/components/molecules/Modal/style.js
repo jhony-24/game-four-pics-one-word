a53a7dc310fc65af7ab084ec701184d5cb0b8c92
jhony <jhonyvega24@gmail.com>
2020-05-06 16:38:40 -0500

    add responsive the 'Modal' component

--- src/components/molecules/Modal/style.js
+++ src/components/molecules/Modal/style.js
@@ -16,7 +16,10 @@ const style = StyleSheet.create({
         width: "90%",
         padding: 10,
         borderRadius: 20,
-        boxShadow: "0 10px 20px rgba(0,0,0,.1)"
+        boxShadow: "0 10px 20px rgba(0,0,0,.1)",
+        "@media screen and (min-width:400px)": {
+            width: "400px",
+        }
     }
 })
 
