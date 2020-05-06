d5a2c77e78ae7eff718b083b1d1d12a35a2dee75
jhony <jhonyvega24@gmail.com>
2020-05-05 19:21:49 -0500

    don't make the text of the component 'NaturalIcon' selectable

--- src/components/atoms/IconNatural/style.js
+++ src/components/atoms/IconNatural/style.js
@@ -1,16 +1,14 @@
 import { StyleSheet } from "aphrodite/no-important"
 
 const style = StyleSheet.create({
-    flex : {
-        justifyContent:"none",
-        padding:"10px",
-        width:"auto"
+    flex: {
+        justifyContent: "none",
+        padding: "10px",
+        width: "auto"
     },
-    iconic : {
-
-    },
-    texticon : {
-        fontWeight : "bold",
+    texticon: {
+        fontWeight: "bold",
+        userSelect: "none",
     }
 })
 
