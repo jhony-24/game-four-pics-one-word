3db0b6468322c3fdf3767309509704593ccbf4ea
jhony <jhonyvega24@gmail.com>
2020-05-06 17:56:40 -0500

    delete option to dark mode

--- src/containers/SettingsApp/SettingsApp.jsx
+++ src/containers/SettingsApp/SettingsApp.jsx
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import RowSetting from './subcomponents/RowSetting';
 import Text from 'src/components/atoms/Text/Text';
-import RowDarkMode from './subcontainers/RowDarkMode';
 import RowSoundActive from './subcontainers/RowSoundActive';
 import RowRename from './subcontainers/RowRename';
 import { connect } from 'react-redux';
@@ -22,7 +21,6 @@ class SettingsApp extends Component {
           detail={<Text color="black">10 publicaciones</Text>}
           src="https://cdn.pixabay.com/photo/2020/04/21/10/26/animal-5072537__340.jpg"
         />
-        <RowDarkMode />
         <RowSoundActive />
         <RowRename defaultUsername={username} />
       </Fragment>
