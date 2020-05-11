9f22c2ef252db6d5a8ba52884ae0796eb0e2f71b
jhony <jhonyvega24@gmail.com>
2020-05-11 17:42:14 -0500

    create button to change my picture of profile

--- src/containers/SettingsApp/subcomponents/HeaderSettings.jsx
+++ src/containers/SettingsApp/subcomponents/HeaderSettings.jsx
@@ -3,6 +3,10 @@ import Container from "src/components/dom/Container";
 import Avatar from "src/components/atoms/Avatar";
 import Flex from "src/components/dom/Flex";
 import Text from "src/components/atoms/Text";
+import { IoIosCamera } from "react-icons/io";
+import Position from "src/components/dom/Position";
+import { motion as HoverAvatar } from "framer-motion";
+import { useState } from "react";
 
 const style = {
     flex: {
@@ -16,15 +20,41 @@ const style = {
         margin: "5px 0"
     },
     marginAvatar: {
-        margin: "2em 0 10px"
+        margin: "2em 0 10px",
+        position: "relative",
     }
 }
 
+
 const HeaderSettings = ({ avatar, title, details }) => {
+    const [hover, setHover] = useState(false);
+    const defineSetHover = (value) => () => {
+        setHover(value);
+    }
     return (
         <Container>
             <Flex styles={style.marginAvatar}>
                 <Avatar src={avatar} size="120px" />
+                <Position position="absolute">
+
+                    <HoverAvatar.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: hover ? 1 : 0 }}
+                        onMouseOver={defineSetHover(true)}
+                        onMouseLeave={defineSetHover(false)}
+                    >
+                        <Avatar
+                            icon={
+                                hover ? (
+                                    <IoIosCamera color="white" size={40} onClick={() => console.log("ok")} />)
+                                    :
+                                    null
+                            }
+                            color="rgba(0,0,0,.3)"
+                            size="120px" />
+                    </HoverAvatar.span>
+
+                </Position>
             </Flex>
             <Flex styles={style.flex}>
                 <Text styles={{ ...style.username, ...style.margin }}>{title}</Text>
