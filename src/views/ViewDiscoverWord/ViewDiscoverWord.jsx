19487f359b3eb5960f7298262df07204555d0633
jhony <jhonyvegacuya24@gmail.com>
2020-06-04 18:02:53 -0500

    add margin to loading view discover data

--- src/views/ViewDiscoverWord/ViewDiscoverWord.jsx
+++ src/views/ViewDiscoverWord/ViewDiscoverWord.jsx
@@ -2,6 +2,8 @@ import React, { lazy } from 'react';
 import LazyLoad from 'src/components/hoc/LazyLoad';
 import NewWordToPlay from './NewWordToPlay';
 import HeaderDiscoverWord from './DiscoverWord/dependencies/headerDiscover/HeaderDiscoverWord';
+import Loading from 'src/components/atoms/Loading/Loading';
+import Position from 'src/components/dom/Position';
 
 const DiscoverWord = lazy(() => import("./DiscoverWord"));
 
@@ -10,7 +12,11 @@ const ViewGuessWord = (props) => {
   return (
     <div className="guess-word">
       <HeaderDiscoverWord />
-      <LazyLoad component={DiscoverWord} state={state} />
+      <LazyLoad component={DiscoverWord} state={state} loading={
+        <Position margin="2in auto">
+          <Loading />
+        </Position>
+      } />
       <NewWordToPlay />
     </div>
   )
